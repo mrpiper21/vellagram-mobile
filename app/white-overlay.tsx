38cd15c92af0ceed238ef8b30465931d4804673b
Bernard <benbaah104@gmail.com>
2025-05-26 20:56:34 +0000

    update

--- app/white-overlay.tsx
+++ app/white-overlay.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { View } from 'react-native'
+import { Colors } from "@/constants/Colors";
+import React from "react";
+import { Text, View } from "react-native";
 
 const WhiteOverlay = () => {
-  return (
-    <View style={{flex: 1, backgroundColor: "white"}}>
-    </View>
-  )
-}
+	return (
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: "white",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<Text
+				style={{ color: Colors.light.text, fontSize: 24, fontWeight: "600" }}
+			>
+				Vedify
+			</Text>
+		</View>
+	);
+};
 
-export default WhiteOverlay
+export default WhiteOverlay;
